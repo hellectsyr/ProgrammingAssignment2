d601422a957d1c80661d2c08e291b90069a41b02
Hellectsyr <tantichrystelle@gmail.com>
2015-08-23 14:20:42 +0800

    Second Commit
    Better indentation

--- cachematrix.R
+++ cachematrix.R
@@ -6,15 +6,15 @@
 ## The first function makeCacheMatrix create a special"matrix", which is really a list containing a function to: 1. set the value of the matrix 2. get the value of the matrix 3. set the value of the inverse 4. get the value of the inverse
 
 makeCacheMatrix <- function(X = matrix()) {
-inv <- NULL
-set <- function(Y){
-	X <<- Y
-	inv <<- NULL
-}
-get <- function() X
-setinverse <- function(inverse) inv <<- inverse
-getinverse <- function() inv
-list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+        inv <- NULL
+        set <- function(Y){
+                X <<- Y
+                inv <<- NULL
+        }
+        get <- function() X
+        setinverse <- function(inverse)inv <<- inverse
+        getinverse <- function() inv
+        list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 ## Write a short comment describing this function
@@ -24,13 +24,13 @@ list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 cacheSolve <- function(X, ...){
 	inv <- X$getinverse()
 	if(!is.null(inv)){
-	message("matrix is in memory")
-	return(inv)
+	        message("matrix is in the memory")
+	        return(inv)
 	}
-data <- X$get()
-inverse <- solve(data, ...)
-X$setinverse(inv)
-inv
+	data <- X$get()
+	inverse <- solve(data, ...)
+	X$setinverse(inv)
+	inv
 }
 
 
